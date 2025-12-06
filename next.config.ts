613e35c0c6546b42a4051ac76766466c2fbd4240
rathichandar <rathichandar@gmail.com>
2025-12-05 23:57:40 -0800

    change next confug

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      '@react-native-async-storage/async-storage': false,
+      'react-native': false,
+      'react-native-crypto': false,
+      'react-native-randombytes': false,
+    };
+    
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    
+    return config;
+  },
 };
 
 export default nextConfig;
